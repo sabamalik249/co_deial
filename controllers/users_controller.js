6f102e3f4abc2e4c6dcb62f4915a4c695db9510e
sabamalik249 <sabamalik249@gmail.com>
2021-11-06 18:14:03 +0530

    manual authetication till sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/users');
+
+
 module.exports.profile= function(req,res){
     return res.render('users_profile',{
         title: "user profile"
@@ -22,7 +25,23 @@ module.exports.signIn = function (req, res) {
 
 //get the sign_up dat
 module.exports.create=function(req,res){
-    //todo
+   if(req.body.password != req.body.confirm_password){
+       return res.redirect('back');
+   }
+   User.findOne({email: req.body.email},function(err,user){
+       if(err){console.log('error in finding the user in sign up');return }
+
+       if(!user){
+           User.create(req.body, function(err,user){
+               if(err){console.log("error in creating user while signing up"); return;}
+
+               return res.redirect('/users/sign-in');
+           })
+       }
+       else{
+           return res.redirect('back');
+       }
+   })
 }
 
 module.exports.createSession = function (req, res) {
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express=require('express');
-const caakieParser=require('cookie-parser');
+const cookieParser=require('cookie-parser');
 const app=express();
 const port=8000;
 const expressLayouts = require('express-ejs-layouts');
 const db=require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
@@ -15,12 +19,7 @@ app.set('layout extractScripts', true);
 
 
 
-const cookieParser = require('cookie-parser');
 
-
-app.use(express.urlencoded());
-
-app.use(cookieParser());
 //use express router
 app.use('/',require('./routes'));
 
